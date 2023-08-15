c012d53ffc48c38b85c9f984a3c324f85c966b1e
Artem Bykov <bykovaa94@mail.ru>
2023-08-15 11:08:33 +0300

    fix

--- src/commons/ApiProvider.js
+++ src/commons/ApiProvider.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect, Component } from 'react';
 import { useDispatch } from 'react-redux';
 import SocketContext from "../context/socketContext";
 import { addEvents } from '../slice/eventStoreSlice';
